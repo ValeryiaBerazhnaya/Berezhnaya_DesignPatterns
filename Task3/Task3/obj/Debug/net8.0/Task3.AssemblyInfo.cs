a0e9946298fc17f5e13f9297f512a385bd256576
valeryiaBerazhnaya <valeryia.berazhnaya@student.ehu.lt>
2025-05-05 13:08:53 +0300

    1

--- Task3/Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
+++ Task3/Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+403b9d25a5f3fe45ffd40226b8def37953c22b78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
